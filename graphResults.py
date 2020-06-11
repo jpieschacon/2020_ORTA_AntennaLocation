86dfe9f3cac84ce9b91896ce0d8eb8341dccd275
jpieschacon <juaranga95hotmail.com>
2020-06-11 21:35:55 +0200

    Reformat file to remove white spaces.

--- graphResults.py
+++ graphResults.py
@@ -18,23 +18,23 @@ class Plot:
             df_aux = self.df[self.df['method'] == method]
             if variable in 'Execution Time':
                 var = 'time'
-                df_aux_mean = pd.DataFrame({'rows' : [],'columns' : [],'time' : []})
-                df_solver_mean = pd.DataFrame({'rows' : [],'columns' : [],'time' : []})
-                n=0
+                df_aux_mean = pd.DataFrame({'rows': [], 'columns': [], 'time': []})
+                df_solver_mean = pd.DataFrame({'rows': [], 'columns': [], 'time': []})
+                n = 0
                 for i in df_aux['rows'].unique():
                     for j in df_aux['columns'].unique():
-                        df_aux_mean=df_aux_mean.append(df_aux[(df_aux['rows'] == i) & (df_aux['columns'] == j)].agg({'time' : ['mean']}))
-                        df_solver_mean=df_solver_mean.append(self.df_solver[(self.df_solver['rows'] == i) & (self.df_solver['columns'] == j)].agg({'time' : ['mean']}))
-                        df_aux_mean['rows'][n]=i
-                        df_solver_mean['rows'][n]=i
-                        df_aux_mean['columns'][n]=j
-                        df_solver_mean['columns'][n]=j
-                        n+=1
+                        df_aux_mean = df_aux_mean.append(df_aux[(df_aux['rows'] == i) & (df_aux['columns'] == j)].agg({'time': ['mean']}))
+                        df_solver_mean = df_solver_mean.append(self.df_solver[(self.df_solver['rows'] == i) & (self.df_solver['columns'] == j)].agg({'time': ['mean']}))
+                        df_aux_mean['rows'][n] = i
+                        df_solver_mean['rows'][n] = i
+                        df_aux_mean['columns'][n] = j
+                        df_solver_mean['columns'][n] = j
+                        n += 1
                 top = np.divide(df_aux_mean[var].values, df_solver_mean[var].values)
                 # top = abs(self.df_solver[var].values - df_aux[var].values)
             elif variable in 'Objective Function Ratio':
                 var = 'of'
-                top = np.divide(df_aux[var][df_aux['seed']==0].values, self.df_solver[var][self.df_solver['seed']==0].values)
+                top = np.divide(df_aux[var][df_aux['seed'] == 0].values, self.df_solver[var][self.df_solver['seed'] == 0].values)
             else:
                 top = 0
             bottom = np.zeros_like(top)
@@ -49,7 +49,7 @@ class Plot:
                 rgba = [cmap(k) for k in top]
             m = cm.ScalarMappable(norm=norm, cmap=cmap)
             fig.colorbar(m, ax=ax, shrink=0.8)
-            ax.bar3d(df_aux['rows'][df_aux['seed']==0], df_aux['columns'][df_aux['seed']==0], bottom, width, depth, top, color=rgba, shade=True)
+            ax.bar3d(df_aux['rows'][df_aux['seed'] == 0], df_aux['columns'][df_aux['seed'] == 0], bottom, width, depth, top, color=rgba, shade=True)
             ax.set_title(method)
             ax.set_xlim([3, 10])
             ax.set_ylim([3, 10])
